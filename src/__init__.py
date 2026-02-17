95218ab76da620f25301403d3ec89b4cae497add
Cusox <cusoxlee@gmail.com>
2026-02-17 22:30:10 +0800

    feat: search book by title

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 import json
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from urllib.request import Request, urlopen
 
 from calibre.ebooks.metadata.book.base import Metadata
@@ -9,6 +10,8 @@ BANGUMI_BASE_URL = "https://bangumi.tv/"
 BANGUMI_API_URL = "https://api.bgm.tv/v0/"
 BANGUMI_SUBJECT_URL = BANGUMI_BASE_URL + "subject/%s"
 BANGUMI_API_SUBJECT_URL = BANGUMI_API_URL + "subjects/%s"
+BANGUMI_API_SUBJECTS_URL = BANGUMI_API_URL + "subjects/%s/subjects"
+BANGUMI_API_SEARCH_URL = BANGUMI_API_URL + "search/subjects"
 PLUGIN_VERSION = (1, 0, 0)
 
 CONFIG = {
@@ -72,7 +75,7 @@ class BangumiMetadata(Source):
 
         return headers
 
-    def _parse_infobox(self, infobox, keys):
+    def _parse_infobox(self, infobox, keys, only_first=False):
         matches = {}
 
         if not infobox:
@@ -94,7 +97,10 @@ class BangumiMetadata(Source):
                 else:
                     result.append(matches[key])
 
-        return result
+        if result and only_first:
+            return result[0]
+        else:
+            return result
 
     def _parse_tags(self, tags):
         if not tags:
@@ -116,14 +122,17 @@ class BangumiMetadata(Source):
         )
         book["tags"] = self._parse_tags(data.get("tags", []))
         book["pubdate"] = parse_date(
-            data["date"] or self._parse_infobox(data.get("infobox", []), ["发售日"])[0]
+            data["date"]
+            or self._parse_infobox(data.get("infobox", []), ["发售日"], only_first=True)
         )
-        book["publisher"] = self._parse_infobox(data.get("infobox", []), ["出版社"])[0]
+        book["publisher"] = self._parse_infobox(
+            data.get("infobox", []), ["出版社"], only_first=True
+        )
         book["comments"] = data.get("summary", "")
         book["rating"] = data.get("rating", {}).get("score", 0)
         book["identifier:isbn"] = self._parse_infobox(
-            data.get("infobox", []), ["ISBN"]
-        )[0]
+            data.get("infobox", []), ["ISBN"], only_first=True
+        )
         book["identifier:bgm"] = data["id"]
 
         return book
@@ -141,7 +150,7 @@ class BangumiMetadata(Source):
 
         return mi
 
-    def _query_subject(self, bangumi_id):
+    def _query_subject(self, log, bangumi_id):
         url = BANGUMI_API_SUBJECT_URL % bangumi_id
         headers = self._get_headers()
 
@@ -154,9 +163,70 @@ class BangumiMetadata(Source):
         data = json.loads(raw_data)
 
         book = self._parse_data(data)
-        mi = self._to_metadata(book)
 
-        return mi
+        return book
+
+    def _query_related_subjects(self, log, bangumi_id):
+        url = BANGUMI_API_SUBJECTS_URL % bangumi_id
+        headers = self._get_headers()
+
+        res = urlopen(Request(url, headers=headers, method="GET"))
+        if res.getcode() == 200:
+            raw_data = res.read()
+        else:
+            return []
+
+        bangumi_ids = []
+
+        data = json.loads(raw_data)
+        for item in data:
+            if item["type"] == 1:
+                bangumi_ids.append(item["id"])
+
+        return bangumi_ids
+
+    def _search_by_title(self, log, title):
+        payload = {
+            "keyword": title,
+            "filter": {
+                "type": [1],
+                "nsfw": True,
+            },
+        }
+
+        url = BANGUMI_API_SEARCH_URL
+        headers = self._get_headers()
+
+        req_body = json.dumps(payload).encode("utf-8")
+        res = urlopen(Request(url, headers=headers, method="POST", data=req_body))
+        if res.getcode() == 200:
+            raw_data = res.read()
+        else:
+            return []
+
+        data = json.loads(raw_data)
+
+        bangumi_ids = [item["id"] for item in data.get("data", [])]
+        if not bangumi_ids:
+            log.info("未找到匹配的 Bangumi 条目")
+            return []
+
+        expanded_ids = set(bangumi_ids)
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            related_ids = executor.map(
+                lambda id: self._query_related_subjects(log, id), bangumi_ids
+            )
+
+            expanded_ids.update(rel_id for sublist in related_ids for rel_id in sublist)
+
+        log.info(f"找到{len(expanded_ids)}个匹配的 Bangumi 条目，正在查询详细信息...")
+
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            books = executor.map(lambda id: self._query_subject(log, id), expanded_ids)
+
+            return list(books)
+
+        return []
 
     def get_book_url(self, identifiers):
         bangumi_id = identifiers.get("bgm", None)
@@ -173,18 +243,27 @@ class BangumiMetadata(Source):
         identifiers={},
         timeout=30,
     ):
+        books = []
+
         bangumi_id = identifiers.get("bgm", None)
         if bangumi_id is not None:
             log.info("正在查询 Bangumi ID %s...", bangumi_id)
 
-            book = self._query_subject(bangumi_id)
+            book = self._query_subject(log, bangumi_id)
 
             log.info("查询 Bangumi ID %s: %s", bangumi_id, "成功" if book else "失败")
 
-            if book:
-                result_queue.put(book)
+            books.append(book)
         else:
             log.info("未提供 Bangumi ID")
+            log.info("通过 Title 进行查询...")
+
+            books = self._search_by_title(log, title)
+
+        for book in books:
+            mi = self._to_metadata(book)
+
+            result_queue.put(mi)
 
 
 if __name__ == "__main__":
@@ -197,14 +276,20 @@ if __name__ == "__main__":
     test_identify_plugin(
         BangumiMetadata.name,
         [
+            # (
+            #     {
+            #         "identifiers": {"bgm": "136517"},
+            #         "title": "OVERLORD",
+            #     },
+            #     [
+            #         title_test("オーバーロード (1)", exact=False),
+            #     ],
+            # ),
             (
                 {
-                    "identifiers": {"bgm": "136517"},
                     "title": "OVERLORD",
                 },
-                [
-                    title_test("オーバーロード (1)", exact=False),
-                ],
+                [],
             ),
         ],
     )
